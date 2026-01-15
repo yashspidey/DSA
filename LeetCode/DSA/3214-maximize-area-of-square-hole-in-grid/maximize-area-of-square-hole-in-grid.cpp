c2ec368ce951190e3691f7c46043ca1468f1bc10
Yash <yashpawar2995@gmail.com>
2026-01-15 13:50:03 +0530

    Time: 3 ms (14.78%) | Memory: 31.9 MB (45.22%) - LeetSync

--- LeetCode/DSA/3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
+++ LeetCode/DSA/3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -3,8 +3,12 @@ public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars,
                                vector<int>& vBars) {
 
+// sort the inputs
+
         sort(begin(hBars), end(hBars));
         sort(begin(vBars), end(vBars));
+
+// initialize variables
 
         int maxConsecutiveHbars = 1;
         int maxConsecutiveVbars = 1;
@@ -12,6 +16,8 @@ public:
         int consecutiveVbars = 1;
         int h = hBars.size();
         int v = vBars.size();
+
+// find maxconsecutive hbars
 
         for (int i = 1; i < h; i++) {
             if (hBars[i] - hBars[i - 1] == 1) {
@@ -22,6 +28,8 @@ public:
             maxConsecutiveHbars = max(maxConsecutiveHbars, consecutiveHbars);
         }
 
+// find maxconsecutivevbars
+
         for (int i = 1; i < v; i++) {
             if (vBars[i] - vBars[i - 1] == 1) {
                 consecutiveVbars++;
@@ -30,8 +38,11 @@ public:
             }
             maxConsecutiveVbars = max(maxConsecutiveVbars, consecutiveVbars);
         }
+// find side
 
         int side = min(maxConsecutiveHbars + 1, maxConsecutiveVbars + 1);
+
+// find and return area
 
         return side * side;
     }
