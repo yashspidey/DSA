5d34f8f68c57fb603d2d7f34be57d84b63c2f57e
Yash <yashpawar2995@gmail.com>
2026-01-19 00:02:04 +0530

    Time: 0 ms (100%), Space: 29.4 MB (89.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,8 +7,38 @@ public:
             return;
         }
 
-        reverse(nums.begin(), nums.begin() + n);
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.begin() + n);
+        // reverse(nums.begin(), nums.begin() + n); // 0,n
+        // reverse(nums.begin(), nums.begin() + k); // 0, k
+        // reverse(nums.begin() + k, nums.begin() + n); // k, n
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i <= j) {
+
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+
+        i = 0;
+        j = k - 1;
+
+        while (i <= j) {
+
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+
+        i = k;
+        j = n - 1;
+
+        while (i <= j) {
+
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
     }
 };
