8a692efbdad132cf594d0765b1acd4d999b9dcf7
Yash <yashpawar2995@gmail.com>
2026-01-20 22:52:38 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.11%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans;
-        int lastPos = -1;
-        int firstPos = INT_MAX;
+    int rightMostPos(int s, int e, vector<int>& nums, int target) {
 
-        for (int i = 0; i < n; i++) {
+        int mid = s + (e - s) / 2;
+        int rightMost = -1;
 
-            if (nums[i] == target) {
+        while (s <= e) {
 
-                lastPos = max(i, lastPos);
-                firstPos = min(i, firstPos);
+            if (nums[mid] == target) { // 5,7,7,8,8,10
+                rightMost = mid;
+                s = mid + 1;
+
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
+            mid = s + (e - s) / 2;
         }
+        return rightMost;
+    }
+    int leftMostPos(int s, int e, vector<int>& nums, int target) {
 
-if(firstPos == INT_MAX){
-firstPos = -1;
-}
+        int mid = s + (e - s) / 2;
+        int leftMost = -1;
 
-        return {firstPos, lastPos};
+        while (s <= e) {
+
+            if (nums[mid] == target) { // 5,7,7,8,8,8
+                leftMost = mid;
+
+                e = mid - 1;
+
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return leftMost;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        int s = 0;
+        int e = n - 1;
+        int ending = rightMostPos(s, e, nums, target);
+        int starting = leftMostPos(s, e, nums, target);
+
+        return {starting, ending};
     }
 };
