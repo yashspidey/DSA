6d18ae6aa3b4696baf26949acb75a1a0bd75d8a2
Yash <yashpawar2995@gmail.com>
2026-01-31 18:34:06 +0530

    Time: 0 ms (100%), Space: 19.9 MB (24.67%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -3,15 +3,22 @@ public:
     char nextGreatestLetter(vector<char>& letters, char target) {
 
         int n = letters.size();
-        char ans = '{'; // INT_MAX wont work here, so we know all array elements
-                        // are guaranteed lowercase english letters, so max
-                        // value will be 'z', so we will keep '{' as a
-                        // replacement of INT_MA basically a char version.
+        int s = 0;
+        int e = n - 1;
+        char ans = '{';
 
-        for (int i = 0; i < n; i++) {
+        while (s <= e) {
 
-            if (letters[i] > target) {
-                ans = min(ans, letters[i]);
+            int mid = s + (e - s) / 2;
+            char ch = letters[mid];
+
+            if (ch > target) {
+                ans = ch; // could be possoble ans so we store and search in the
+                          // extreme left until we find answer.
+                e = mid - 1;
+
+            } else {
+                s = mid + 1;
             }
         }
 
