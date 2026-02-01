dbde4f99738b57a47113c5f8709f94cc04512552
Yash <yashpawar2995@gmail.com>
2026-02-01 11:27:19 +0530

    Time: 0 ms (100%), Space: 22.6 MB (51.24%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,33 @@
 class Solution {
-public: // this question is a good fast and slow pointer question 
+public: // this question is a good fast and slow pointer question
     int removeDuplicates(vector<int>& nums) {
 
         int n = nums.size();
         int j = 0; // slow pointer
+        int i = 0; // fast pointer
 
-        for (int i = 0; i < n; i++) {  // i will be our fast pointer
+        // for loop code version ( in such questions for loop is
+        // better, less bug prone, loop bounds are clear in one line
+        // )
+
+        for (int i = 0; i < n; i++) {
 
             if (nums[i] != nums[j]) {
                 j++;
                 nums[j] = nums[i];
             }
         }
+
+        // while (i < n) { // while loop code version
+
+        //     if (nums[i] == nums[j]) {
+        //         i++;
+        //     } else {
+        //         j++;
+        //         nums[j] = nums[i];
+        //         i++;
+        //     }
+        // }
 
         return j + 1;
     }
