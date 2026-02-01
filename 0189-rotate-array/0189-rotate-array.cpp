10b319a6bc8fee8a7ac627987e121eaafce3e7ed
Yash <yashpawar2995@gmail.com>
2026-02-01 13:19:33 +0530

    Time: 0 ms (100%), Space: 29.5 MB (67.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,6 +1,14 @@
 class Solution {
-public:
+public // ONE WAY TO DO THIS QUESTION IS USING STL FUNCTION REVERSE
+
+    // THE OTHER WAY TO DO IS MANUALLY WRITING REVERSE FUNCTION, DOES THE SAME
+    // WORK AS FIRST METHOD BUT WE JUST WRITE REVERSE FUNCTION MANUALLY.
+
+    // THE WORST WAY TO DO IT IS USING AN EXTRA SPACE.
+
+    : //  unsigened means only positive and signed means positive negative both
     void rotate(vector<int>& nums, int k) {
+
         int n = nums.size();
         if (n == k) {
             return;
@@ -8,8 +16,54 @@ public:
 
         k = k % n;
 
-        reverse(begin(nums), begin(nums) + n);
-        reverse(begin(nums), begin(nums) + k);
-        reverse(begin(nums) + k, begin(nums) + n);
+        int i = 0;
+        int j = n - 1;
+
+        while (i <= j) {
+
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+
+        i = 0;
+        j = k - 1;
+
+        while (i <= j) {
+
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+        i = k;
+        j = n - 1;
+
+        while (i <= j) {
+
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+
+        //---------------------------------------------------------------------------
+
+        // int n = nums.size();       // uses extra space so not an optimal
+        // approach k = k % n; vector<int> ans(n);
+
+        // for (int i = 0; i < k; i++) {
+        //     ans[i] = nums[n - k + i];
+        // }
+
+        // for (int i = k; i < n; i++) {
+        //     ans[i] = nums[i - k];
+        // }
+
+        // nums = ans;
+
+        //---------------------------------------------------------------------------
+
+        // reverse(begin(nums), begin(nums) + n);
+        // reverse(begin(nums), begin(nums) + k);
+        // reverse(begin(nums) + k, begin(nums) + n);
     }
 };
