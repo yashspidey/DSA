f88f70f0628e08db137b33585bcfe3c435904584
Yash <yashpawar2995@gmail.com>
2026-02-03 10:11:28 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -14,5 +14,21 @@ public:
         xor1 ^= n; // most optimal approach
 
         return xor1 ^ xor2;
+
+        // another optimal approach
+
+        // int n = nums.size();
+        // int sum1 = 0;
+        // int sum2 = 0;
+
+        // for (int i = 0; i <= n; i++) {
+        //     sum1 += i;
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     sum2 += nums[i];
+        // }
+
+        // return sum1 - sum2;
     }
 };
