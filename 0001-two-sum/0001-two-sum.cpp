571b15eb947abba07c6601d4a0451a151d85de7d
Yash <yashpawar2995@gmail.com>
2026-02-06 18:17:54 +0530

    Time: 0 ms (100%), Space: 14.9 MB (18.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,44 +1,52 @@
 class Solution {
-public:
+public: // this problem has 3 approaches
+        // brute - O(n^2) & SC - O(1)
+        // better - O(n*logn) & SC - O(n)
+        // optimal - O(n) & SC - O(n)
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        vector<pair<int, int>> ans;
+        int n = nums.size();
+        unordered_map<int, int> mp;
 
-        for (int i = 0; i < nums.size(); i++) {
-            ans.push_back({nums[i], i});
-        }
+        for (int i = 0; i < n; i++) {
 
-        sort(begin(ans), end(ans));
-
-        int left = 0;
-        int rght = nums.size() - 1;
-
-        while (left < rght) {
-
-            int sum = ans[left].first + ans[rght].first;
-
-            if (sum == target) {
-                return {ans[left].second, ans[rght].second};
-            } else if (sum > target) {
-                rght--;
-            } else {
-                left++;
+            int rem = target - nums[i];
+            if (mp.find(rem) != mp.end()) {
+                return {i, mp[rem]};
             }
+            mp[nums[i]] = i;
         }
 
         return {};
-        // int n = nums.size();
-        // unordered_map<int, int> mp;
 
-        // for (int i = 0; i < n; i++) {
+        // --------------------------------------------------------------------------
 
-        //     int rem = target - nums[i];
-        //     if (mp.find(rem) != mp.end()) {
-        //         return {i, mp[rem]};
-        //     }
-        //     mp[nums[i]] = i;
-        // }
+        // better approach O(n*logn)
 
-        // return {};
+        //  vector<pair<int, int>> ans;
+
+        //         for (int i = 0; i < nums.size(); i++) {
+        //             ans.push_back({nums[i], i});
+        //         }
+
+        //         sort(begin(ans), end(ans));
+
+        //         int left = 0;
+        //         int rght = nums.size() - 1;
+
+        //         while (left < rght) {
+
+        //             int sum = ans[left].first + ans[rght].first;
+
+        //             if (sum == target) {
+        //                 return {ans[left].second, ans[rght].second};
+        //             } else if (sum > target) {
+        //                 rght--;
+        //             } else {
+        //                 left++;
+        //             }
+        //         }
+
+        //         return {};
     }
 };
