97203639356261b99d5470d7c763ee7939e34eac
Yash <yashpawar2995@gmail.com>
2026-02-11 22:20:08 +0530

    Time: 4 ms (53.92%), Space: 14.9 MB (36.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,8 +1,5 @@
 class Solution {
-public: // this problem has 3 approaches
-        // brute - O(n^2) & SC - O(1)
-        // better - O(n*logn) & SC - O(n)
-        // optimal - O(n) & SC - O(n)
+public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         int n = nums.size();
@@ -11,42 +8,14 @@ public: // this problem has 3 approaches
         for (int i = 0; i < n; i++) {
 
             int rem = target - nums[i];
+            
             if (mp.find(rem) != mp.end()) {
                 return {i, mp[rem]};
             }
+
             mp[nums[i]] = i;
         }
 
         return {};
-
-        // --------------------------------------------------------------------------
-
-        // better approach O(n*logn)
-
-        //  vector<pair<int, int>> ans;
-
-        //         for (int i = 0; i < nums.size(); i++) {
-        //             ans.push_back({nums[i], i});
-        //         }
-
-        //         sort(begin(ans), end(ans));
-
-        //         int left = 0;
-        //         int rght = nums.size() - 1;
-
-        //         while (left < rght) {
-
-        //             int sum = ans[left].first + ans[rght].first;
-
-        //             if (sum == target) {
-        //                 return {ans[left].second, ans[rght].second};
-        //             } else if (sum > target) {
-        //                 rght--;
-        //             } else {
-        //                 left++;
-        //             }
-        //         }
-
-        //         return {};
     }
 };
