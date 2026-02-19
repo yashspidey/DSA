7e02e9aa7db33f393b5930f92dbf2e8db12cda29
Yash <yashpawar2995@gmail.com>
2026-02-20 01:22:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,19 +3,30 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         int n = nums.size();
-        unordered_map<int, int> mp;
 
         for (int i = 0; i < n; i++) {
 
-            int rem = target - nums[i];
-            
-            if (mp.find(rem) != mp.end()) {
-                return {i, mp[rem]};
-            }
-
-            mp[nums[i]] = i;
+            ans.push_back({nums[i], i});
         }
 
+        sort(begin(ans), end(ans));
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+
+            int first = ans[i].first;
+            int last = ans[j].first;
+
+            if (first + last == target) {
+                return {ans[i].second, ans[j].second};
+            } else if (first + last < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
         return {};
     }
 };
