f1fbb861ffe0234b1de85c456cbea32c7cada946
Yash <yashpawar2995@gmail.com>
2026-02-24 01:11:21 +0530

    Time: 0 ms (100%), Space: 71.6 MB (97.78%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public: // KADANES ALGORITHM
+    int maxSubArray(vector<int>& nums) {
+
+        int n = nums.size();
+        int sum = 0;
+        int maxSum = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += nums[i];
+
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+};
