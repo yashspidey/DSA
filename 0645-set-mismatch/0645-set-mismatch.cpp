918176596c2375d06e87feea751ef905877c6d98
Yash <yashpawar2995@gmail.com>
2026-03-11 00:44:18 +0530

    Time: 27 ms (27.25%), Space: 34.5 MB (23.5%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
 
         int n = nums.size();
-        vector<int> ans;
         unordered_set<int> st;
         int array_sum = 0;
         int unique_sum = 0;
@@ -25,8 +24,6 @@ public:
         int duplicate = array_sum - unique_sum;
         int missing = original_sum - unique_sum;
 
-        ans.push_back(duplicate);
-        ans.push_back(missing);
-        return ans;
+        return {duplicate, missing};
     }
 };
