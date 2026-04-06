99535e3bc87f432eb8c43d8f0e31187dceca3e90
Yash <yashpawar2995@gmail.com>
2026-04-07 04:58:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (10.69%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -11,14 +11,6 @@ public:
             }
         }
 
-        // for (int i = 0; i < numRows; i++) {
-        //     for (int j = 0; j <= i; j++) {
-        //         if (j != 0 && j != i) {
-        //             ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
-        //         }
-        //     }
-        // }
-
         return ans;
     }
 };
