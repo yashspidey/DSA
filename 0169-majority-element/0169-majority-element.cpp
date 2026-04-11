ef2d2529ccb980f8e4feae88a186b93f609f3d54
Yash <yashpawar2995@gmail.com>
2026-04-11 19:19:00 +0530

    Time: 7 ms (10.89%), Space: 28 MB (87%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -8,12 +8,9 @@ public:
         for (int i = 0; i < n; i++) {
 
             mp[nums[i]]++;
-        }
 
-        for (auto it : mp) {
-
-            if (it.second > n / 2) {
-                return it.first;
+            if (mp[nums[i]] > n / 2) {
+                return nums[i];
             }
         }
 
