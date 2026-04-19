53eb13183c6b65683acaf1acef27b96016b8ac4d
Yash <yashpawar2995@gmail.com>
2026-04-19 18:07:55 +0530

    Time: 0 ms (100%), Space: 31.5 MB (25.13%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -8,18 +8,21 @@ public:
         int maxNum = INT_MIN;
         int minNum = INT_MAX;
 
+        // Build prefix max array
         for (int i = 0; i < n; i++) {
 
             maxNum = max(maxNum, nums[i]);
             prefixMax[i] = maxNum;
         }
 
+        // Build suffix min array
         for (int i = n - 1; i >= 0; i--) {
 
             minNum = min(minNum, nums[i]);
             sufixMin[i] = minNum;
         }
 
+        // Find first smallest stable index
         for (int i = 0; i < n; i++) {
 
             if (prefixMax[i] - sufixMin[i] <= k) {
