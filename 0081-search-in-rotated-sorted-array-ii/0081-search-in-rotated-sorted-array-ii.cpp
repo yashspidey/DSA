609ef818336281d96b31e53e0154eb815d935ebe
Yash <yashpawar2995@gmail.com>
2026-04-29 00:34:51 +0530

    Time: 0 ms (100%), Space: 18 MB (60.85%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,59 +1,15 @@
-
-
 class Solution {
 public:
-    int findPivot(vector<int>& nums, int n, int s, int e) {
+    bool search(vector<int>& nums, int target) {
 
-        while (s < e) {
+        int n = nums.size();
 
-            // calculate mid after skipping duplicates !!
-
-            while (s < e && nums[s] == nums[s + 1]) {
-                s++;
-            }
-
-            while (s < e && nums[e] == nums[e - 1]) {
-                e--;
-            }
-            int mid = s + (e - s) / 2;
-
-            if (nums[mid] > nums[e]) {
-                s = mid + 1;
-            } else {
-                e = mid;
-            }
-        }
-        return s;
-    }
-
-    bool binarySearch(vector<int>& nums, int n, int s, int e, int target) {
-
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] > target) {
-                e = mid - 1;
-            } else if (nums[mid] < target) {
-                s = mid + 1;
-            } else {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
                 return true;
             }
         }
+
         return false;
-    }
-
-    bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int s = 0;
-        int e = n - 1;
-
-        int pivot_index = findPivot(nums, n, s, e);
-
-        bool search = binarySearch(nums, n, s, pivot_index - 1, target);
-
-        if (search) {
-            return search;
-        }
-
-        return binarySearch(nums, n, pivot_index, n - 1, target);
     }
 };
