3b464fe68a97471fcdb2aa722f50eb45703a7f38
Yash <yashpawar2995@gmail.com>
2026-04-28 23:17:08 +0530

    Time: 0 ms (100%), Space: 17.4 MB (88.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,57 +1,55 @@
 class Solution {
 public:
-    int starting(vector<int>& nums, int low, int high, int target) {
-
-        int ans = -1;
+    int lb(vector<int>& nums, int low, int high, int target) {
+        int ans = nums.size();
 
         while (low <= high) {
-
             int mid = low + (high - low) / 2;
 
-            if (nums[mid] == target) {
+            if (nums[mid] >= target) {
                 ans = mid;
                 high = mid - 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
             } else {
-                high = mid - 1;
+                low = mid + 1;
             }
         }
 
         return ans;
     }
 
-    int ending(vector<int>& nums, int low, int high, int target) {
+    int ub(vector<int>& nums, int low, int high, int target) {
 
-        int ans = -1;
+        int ans = nums.size();
 
         while (low <= high) {
-
             int mid = low + (high - low) / 2;
 
-            if (nums[mid] == target) {
+            if (nums[mid] > target) {
                 ans = mid;
-                low = mid + 1;
-
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
-        return ans;
+        return ans - 1;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
 
         int n = nums.size();
+
         int low = 0;
         int high = n - 1;
 
-        int start = starting(nums, low, high, target);
-        int end = ending(nums, low, high, target);
+        int lowb = lb(nums, low, high, target);
+        if (lowb == n || nums[lowb] != target) {
+            return {-1, -1};
+        }
+        int upb = ub(nums, low, high, target);
 
-        return {start, end};
+        
+
+        return {lowb, upb};
     }
 };
