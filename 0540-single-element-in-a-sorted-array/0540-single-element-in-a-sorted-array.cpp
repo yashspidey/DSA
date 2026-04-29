2b81f6f6117cc49873f2434278544e5cf15f81de
Yash <yashpawar2995@gmail.com>
2026-04-29 11:13:56 +0530

    Time: 0 ms (100%), Space: 26.2 MB (67.98%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -25,18 +25,12 @@ public:
                 return nums[mid];
             }
 
-            if (mid % 2 != 0) {
-                if (nums[mid] == nums[mid - 1]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+            if (mid % 2 != 0 && nums[mid] == nums[mid - 1] ||
+                mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
+
+                low = mid + 1;
             } else {
-                if (nums[mid] == nums[mid - 1]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
+                high = mid - 1;
             }
         }
 
