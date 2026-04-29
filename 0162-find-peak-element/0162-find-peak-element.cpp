30ca43c752d11872cf849d5cead6484297bfdcff
Yash <yashpawar2995@gmail.com>
2026-04-29 11:49:40 +0530

    Time: 0 ms (100%), Space: 12.7 MB (39.22%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,29 +8,15 @@ public:
             return 0;
         }
 
-        if (n == 2) {
-            if (nums[0] > nums[1]) {
-                return 0;
-            } else {
-                return 1;
-            }
+        if (nums[0] > nums[1]) {
+            return 0;
         }
 
-        for (int i = 0; i < n; i++) {
+        if (nums[n - 1] > nums[n - 2]) {
+            return n - 1;
+        }
 
-            if (i == 0) {
-                if (nums[i] > nums[i + 1]) {
-                    return i;
-
-                }
-                continue;
-            }
-            if (i == n - 1) {
-                if (nums[i] > nums[i - 1]) {
-                    return i;
-                }
-                continue;
-            }
+        for (int i = 1; i < n - 1; i++) {
 
             if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
                 return i;
