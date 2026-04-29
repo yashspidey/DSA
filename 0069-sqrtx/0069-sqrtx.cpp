baa60ad7177b8c46d073d4040e3f083ca98ae1fb
Yash <yashpawar2995@gmail.com>
2026-04-29 20:42:46 +0530

    Time: 14 ms (10.79%), Space: 8.5 MB (49.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+
+        int ans = 0;
+
+        for (long long i = 0; i <= x; i++) {
+
+            if (i * i <= x) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+
+        return ans;
     }
 };
