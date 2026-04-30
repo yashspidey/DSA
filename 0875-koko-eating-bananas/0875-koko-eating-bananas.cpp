0403f2bc45812d22532cdf2e10e9576856febe9e
Yash <yashpawar2995@gmail.com>
2026-04-30 18:09:33 +0530

    Time: 0 ms (100%), Space: 22.8 MB (97.92%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -19,7 +19,6 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
 
         int low = 1;
-        int ans = 0;
         int high = *max_element(piles.begin(), piles.end());
 
         while (low <= high) {
@@ -29,13 +28,12 @@ public:
             long long hours = calculateHours(piles, mid);
 
             if (hours <= h) {
-                ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
 
-        return ans;
+        return low;
     }
 };
