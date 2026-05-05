f0a3c7db17abbe4c0a8cfceba115d709be16afe7
Yash <yashpawar2995@gmail.com>
2026-05-06 01:41:18 +0530

    Time: 0 ms (100%), Space: 95.2 MB (60.21%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,76 +5,45 @@ public:
         int m = nums1.size();
         int n = nums2.size();
 
-        int i = 0;
-        int j = 0;
-        int count = 0;
-        int idx1 = (m + n) / 2;
-        int idx2 = idx1 - 1;
-        int el1 = -1;
-        int el2 = -1;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                if (count == idx1) {
-                    el1 = nums1[i];
+        int low = 0;
+        int high = m;
+        int left = (m + n + 1) / 2;
+        int len = m + n;
+
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if (mid1 < m) {
+                r1 = nums1[mid1];
+            }
+            if (mid2 < n) {
+                r2 = nums2[mid2];
+            }
+            if (mid1 - 1 >= 0) {
+                l1 = nums1[mid1 - 1];
+            }
+            if (mid2 - 1 >= 0) {
+                l2 = nums2[mid2 - 1];
+            }
+            if (l1 <= r2 && l2 <= r1) {
+                if (len % 2 == 1) {
+                    return max(l1, l2);
                 }
 
-                if (count == idx2) {
-                    el2 = nums1[i];
-                }
-
-                count++;
-                i++;
-
+                return (double)((double)max(l1, l2) + (double)min(r1, r2)) / 2.0;
+            } else if (l1 >= r2) {
+                high = mid1 - 1;
             } else {
-                if (count == idx1) {
-                    el1 = nums2[j];
-                }
-
-                if (count == idx2) {
-                    el2 = nums2[j];
-                }
-
-                count++;
-                j++;
+                low = mid1 + 1;
             }
         }
 
-        while (i < m) {
-            if (count == idx1) {
-                el1 = nums1[i];
-            }
-
-            if (count == idx2) {
-                el2 = nums1[i];
-            }
-
-            count++;
-            i++;
-        }
-
-        while (j < n) {
-            if (count == idx1) {
-                el1 = nums2[j];
-            }
-
-            if (count == idx2) {
-                el2 = nums2[j];
-            }
-
-            count++;
-            j++;
-        }
-
-        int len = (m + n);
-        double median = 0;
-
-        if (len % 2 != 0) {
-            return median = el1;
-        }
-
-        median = (double)(el1 + el2) / 2.0;
-
-        return median;
+        return -1;
     }
 };
