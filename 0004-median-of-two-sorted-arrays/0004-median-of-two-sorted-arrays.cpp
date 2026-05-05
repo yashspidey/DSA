fbdb622215cf0c41423570fecb87920ae61c2a19
Yash <yashpawar2995@gmail.com>
2026-05-05 22:08:44 +0530

    Time: 8 ms (6.45%), Space: 96 MB (9.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -30,11 +30,9 @@ public:
         double median = 0;
 
         if (len % 2 != 0) {
-            median = ans[len / 2];
-        } else {
-            median = ((double)ans[len / 2] + ans[(len / 2) - 1]) / 2.0;
+            return median = ans[len / 2];
         }
-
-        return median;
+        
+        return median = ((double)ans[len / 2] + ans[(len / 2) - 1]) / 2.0;
     }
 };
