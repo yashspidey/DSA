812b4332774ca280792e0c00196be646f680f2d6
Yash <yashpawar2995@gmail.com>
2026-05-06 05:40:44 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,54 +1,25 @@
 class Solution {
 public:
-    bool BinarySearch(vector<int> matrix, int target) {
-
-        int n = matrix.size();
-        int low = 0;
-        int high = n - 1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (target == matrix[mid]) {
-                return true;
-            } else if (target < matrix[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return false;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
         int m = matrix.size();
         int n = matrix[0].size();
+
         int low = 0;
-        int high = m - 1;
+        int high = (m * n) - 1;
 
         while (low <= high) {
 
             int mid = low + (high - low) / 2;
 
-            if (target < matrix[mid][0]) {
+            if (target == matrix[mid / n][mid % n]) {
+                return true;
+            } else if (target < matrix[mid / n][mid % n]) {
                 high = mid - 1;
-            } else if (target > matrix[mid][n - 1]) {
-                low = mid + 1;
             } else {
-
-                return BinarySearch(matrix[mid], target);
+                low = mid + 1;
             }
         }
-
-        // for (int i = 0; i < m; i++) {
-
-        //     if (matrix[i][0] <= target && target <= matrix[i][n - 1]) {
-        //         return BinarySearch(matrix[i], target);
-        //     }
-        // }
 
         return false;
     }
