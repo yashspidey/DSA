19ce39f2db8e99d4d26eb5ca9c4dead1eed231dc
Yash <yashpawar2995@gmail.com>
2026-05-12 20:09:53 +0530

    Time: 0 ms (100%), Space: 7.6 MB (87.34%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,10 +2,21 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
 
-        for (long long i = 1; i <= num; i++) {
+        int low = 1;
+        int high = num;
 
-            if (i * i == num) {
+        while (low <= high) {
+
+            long long mid = low + (high - low) / 2;
+
+            long long sq = mid * mid;
+
+            if (sq == num) {
                 return true;
+            } else if (sq < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
