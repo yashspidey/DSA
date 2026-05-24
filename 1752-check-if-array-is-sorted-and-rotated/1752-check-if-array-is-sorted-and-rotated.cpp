c7267c45d3ef35f7a3723718fabdb22056640d91
Yash <yashpawar2995@gmail.com>
2026-05-24 06:42:21 +0530

    Time: 1 ms (3.71%), Space: 11.9 MB (5.18%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
-    bool checkIfSorted(vector<int>& nums) {
-
-        int n = nums.size();
-
-        for (int i = 0; i < n - 1; i++) {
-
-            if (nums[i] > nums[i + 1]) {
-                break;
-            }
-
-            if (i == n - 2 && nums[i] <= nums[i + 1]) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    void rotate(vector<int>& nums, int k) {
-
-        int n = nums.size();
-
-        reverse(nums.begin(), nums.begin() + (k - 1));
-        reverse(nums.begin() + k, nums.end());
-        reverse(nums.begin(), nums.end());
-    }
-
     bool check(vector<int>& nums) {
 
         int n = nums.size();
+        vector<int> sorted(n);
 
-        if (n == 1) {
-            return true;
-        }
+        for (int i = 0; i < n; i++) {
 
-        if (checkIfSorted(nums)) {
-            return true;
-        }
+            int x = i;
 
-        int count = 1;
-        int rotations = 1;
+            for (int j = 0; j < n; j++) {
 
-        while (count < n) {
-
-            rotate(nums, rotations);
-
-            bool valid = checkIfSorted(nums);
-
-            if (valid) {
-                return true;
+                sorted[(n - x + j) % n] = nums[j];
             }
 
-            count++;
+            bool valid = true;
+
+            for (int k = 0; k < sorted.size() - 1; k++) {
+
+                if (sorted[k] > sorted[k + 1]) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid == true) {
+                return true;
+            }
         }
 
         return false;
