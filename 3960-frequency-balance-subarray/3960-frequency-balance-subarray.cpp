cda343e7188fada8db232a678e1be90f5e0039d4
Yash <yashpawar2995@gmail.com>
2026-06-15 10:56:05 +0530

    Time: 2254 ms (10%), Space: 472.6 MB (10%) - LeetHub

---
+++ 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getLength(vector<int>& nums) {
+        int len = 0, n = nums.size();
+        map<int, int> a, b;
+        for (int i = 0; i < n; i++) {
+            a.clear();
+            b.clear();
+            for (int j = i; j < n; j++) {
+
+                if (a.find(nums[j]) != a.end()) {
+                    b[a[nums[j]]]--;
+                    if (b[a[nums[j]]] == 0) {
+                        b.erase(a[nums[j]]);
+                    }
+                }
+
+                a[nums[j]]++;
+                b[a[nums[j]]]++;
+                if ((b.size() == 2 &&
+                     b.begin()->first * 2 == b.rbegin()->first) ||
+                    (b.size() == 1 && a.size() == 1)) {
+                    len = max(len, j - i + 1);
+                }
+            }
+        }
+        return len;
+    }
+};
