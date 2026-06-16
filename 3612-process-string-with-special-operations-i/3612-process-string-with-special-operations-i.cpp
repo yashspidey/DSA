43806cbe7099c925ed7bf97e0c096e97299d6f71
Yash <yashpawar2995@gmail.com>
2026-06-16 23:57:51 +0530

    Time: 0 ms (100%), Space: 39.8 MB (69.7%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -5,21 +5,7 @@ public:
     string processStr(string s) {
 
         string result;
-        bool valid = false;
-
-        for (char ch : s) {
-
-            int ascival = ch - 'a';
-
-            if (97 <= ch && ch <= 122) {
-                valid = true;
-                break;
-            }
-        }
-
-        if (!valid) {
-            return "";
-        }
+       
 
         // a # b % *
 
