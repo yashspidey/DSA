4a76e83c39ccd2211fd92663aea30ff762b0cea3
Yash <yashpawar2995@gmail.com>
2026-06-27 11:36:24 +0530

    Time: 16 ms (75.33%), Space: 288.4 MB (42.25%) - LeetHub

---
+++ 3964-minimum-lights-to-illuminate-a-road/3964-minimum-lights-to-illuminate-a-road.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int minLights(vector<int>& lights) {
+        // [0,0,0,2,0]
+
+        vector<long long> litup(lights.size() + 1, 0);
+        int n = lights.size();
+
+        for (int i = 0; i < lights.size(); i++) {
+
+            if (lights[i] != 0) {
+
+                int v = lights[i];
+
+                litup[max(0, i - v)] += v;
+                litup[min(n - 1, i + v) + 1] -= v;
+            }
+        }
+
+        for (int i = 1; i < lights.size(); i++) {
+            litup[i] += litup[i - 1];
+        }
+        litup.pop_back();
+
+        // 0 , 0, 1 ,0 ,0, 1, 1, 1
+        int bulb = 0;
+        int counter = 0;
+        for (int i = 0; i < litup.size(); i++) {
+            if (litup[i] == 0) {
+                counter++;
+            } else if (litup[i] >= 1) {
+                if (counter > 0) {
+                    bulb++;
+                    counter = 0;
+                }
+            }
+
+            if (counter == 3) {
+                counter = 0;
+                bulb++;
+            }
+        }
+
+        if (counter) {
+            bulb++;
+        }
+
+        return bulb;
+    }
+};
