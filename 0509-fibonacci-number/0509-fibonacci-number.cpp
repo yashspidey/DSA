4d2500c3ac1541e43b3d296e08175110a1b21dd5
Yash <yashpawar2995@gmail.com>
2026-08-08 14:36:03 +0530

    Time: 15 ms (22.61%), Space: 7.9 MB (38.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-
-        if (n <= 1) {
-            return n;
+        if (n == 0) {
+            return 0;
         }
 
-        int last = fib(n - 1);
-        int slast = fib(n - 2);
+        if (n == 1) {
+            return 1;
+        }
 
-        return last + slast;
+        return fib(n - 1) + fib(n - 2);
     }
 };
